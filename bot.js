6ff476ce16c12dec372bbcc65bcaad4c0cf534a4
Legendking115 <51090920+Legendking115@users.noreply.github.com>
2019-05-28 17:03:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1309,7 +1309,7 @@ client.on("message", message => {
  });
 
 
-const devs = ['349095859859881984'];
+const devs = ['551435927868932099'];
 const adminprefix = "!";
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -1337,30 +1337,6 @@ if (message.content.startsWith(adminprefix + 'st')) {
 
 
 
-const deves = ['551435927868932099'];
-const adminprefix = "!";
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!deves.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else 
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'avatarr')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
-});
 
 
 
